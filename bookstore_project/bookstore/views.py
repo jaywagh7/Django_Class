12cd0ab61610a19a0cc811edbe6b6b2484b79720
jaywagh7 <jayeshwagh665@gmail.com>
2025-01-17 15:34:41 +0530

    Add

--- bookstore_project/bookstore/views.py
+++ bookstore_project/bookstore/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-# Create your views here.
+from .forms import BookForm
 
+# Existing views for list and detail
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'book_list.html', {'books': books})
+    return render(request, 'bookstore/book_list.html', {'books': books})
 
 def book_detail(request, book_id):
-    book = Book.objects.get(id=book_id)
-    return render(request, 'book_detail.html', {'book': book})
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'bookstore/book_detail.html', {'book': book})
+
+# New view for updating a book
+def book_update(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookstore/book_form.html', {'form': form, 'title': 'Update Book'})
+
+# New view for deleting a book
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookstore/book_confirm_delete.html', {'book': book})
